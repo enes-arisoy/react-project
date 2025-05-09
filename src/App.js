663a9c55984830e6128d131d538f32dbc4c93228
enes-arisoy <enes.arisoy.85@gmail.com>
2025-05-09 17:25:14 +0300

    react-project started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Product from "./Pages/Product";
+import Header from "./Components/Header";
+import ProductCard from "./Components/ProductCard";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/productCard" element={<ProductCard />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className='flex items-center font-bold text-3xl'>
+      Product
+    </div>
+  )
+}
+
+export default Product
